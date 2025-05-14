ee8f7e3514deb25128173f9e468f0982725a7723
Binada-SH <binadasasmika@gmail.com>
2025-05-14 16:27:25 +0530

    Movies importing and displaying in the application

--- movie_dashboard/src/App.tsx
+++ movie_dashboard/src/App.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-
+import movieData from './movies.json';
 import './App.css';
 
+export interface Movie {
+  id: string,
+  title: string,
+  posterUrl: string,
+  genre: string[],
+  rating: number,
+  year: number,
+  description: string,
+  trailerUrl: string
+}
+
 function App() {
+  const movies: Movie[] = movieData as Movie[];
   return (
     <div className="App">
+      <h1 className="wite_name">Cine Stream</h1>
+      <div className="movie_container">
+        {movies.map((movies) => (
+          <div key={movies.id}>
+            <img src={movies.posterUrl} alt={movies.title} className="movie_poster" />
+            <h2>{movies.title}</h2>
+            <p>{movies.genre.join(', ')}</p>
+            <p>{movies.description}</p>
+            <p>{movies.year}</p>
+          </div>
+        ))}
+      </div>
+
+
+
+
+
+
+
     </div>
   );
 }
