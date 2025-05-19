be6f284c11b8d7beb3482cd41eb43869b86c2ffa
Binada-SH <binadasasmika@gmail.com>
2025-05-19 21:15:28 +0530

    Movie database displaying on app

--- movie_dashboard/src/App.tsx
+++ movie_dashboard/src/App.tsx
@@ -1,11 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+import movieData from './movies.json'
+
+export interface Movie {
+    id: string;
+    title: string;
+    posterUrl: string;
+    genre: string[];
+    rating: number;
+    year: number;
+    description: string;
+    trailerUrl: string;
+  }
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const movies: Movie[] = movieData as Movie[];
+
   return (
-    <div className="app"></div>
+    <div className="app">
+      <div className="movie_container">
+        {movies.map((movies) => (
+          <div key={movies.id}>
+            <img src={movies.posterUrl} alt="movie_poster"/>
+            <h1>{movies.title}</h1>
+            <p>{movies.genre.join(', ')}</p>
+            <p>{movies.description}</p>
+            <p>{movies.year}</p>
+
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
