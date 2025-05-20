13e7db0cb1151f22bacdf67284aad935b3e2a45c
Binada-SH <binadasasmika@gmail.com>
2025-05-20 23:52:01 +0530

    Movie grid creation and ui improvments

--- movie_dashboard/src/App.tsx
+++ movie_dashboard/src/App.tsx
@@ -1,51 +1,57 @@
-import { useState } from 'react'
-import './App.css'
-import movieData from './movies.json'
+import { useState } from 'react';
+import './App.css';
+import movieData from './movies.json';
 import Drawer from './Components/drawer/drawer';
 import Search from './Components/search/search';
 import Navbar from './Components/navbar/navbar';
 import Account from './Components/account/account';
 
 export interface Movie {
-    id: string;
-    title: string;
-    posterUrl: string;
-    genre: string[];
-    rating: number;
-    year: number;
-    description: string;
-    trailerUrl: string;
-  }
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  const movies: Movie[] = movieData as Movie[];
-
-  return (
-    <div className='bg-[#060606] w-full min-h-full '>
-      <div className='flex h-screen'>
-        <Drawer/>
-      <div className='flex flex-row'>
-        <Search/>
-        <Navbar/>
-        <Account/>
-      <div className="movie_container">
-        {movies.map((movies) => (
-          <div key={movies.id}>
-            <img src={movies.posterUrl} alt="movie_poster"/>
-            <h1>{movies.title}</h1>
-            <p>{movies.genre.join(', ')}</p>
-            <p>{movies.description}</p>
-            <p>{movies.year}</p>
-
-          </div>
-        ))}
-        </div>
-      </div>
-      </div>
-    </div>
-  )
+  id: string;
+  title: string;
+  posterUrl: string;
+  genre: string[];
+  rating: number;
+  year: number;
+  description: string;
+  trailerUrl: string;
 }
 
-export default App
+function App() {
+  const [count, setCount] = useState(0);
+  
+  const movies = movieData as Movie[];
+
+  return (
+    <div className="bg-[#060606] w-full min-h-full">
+      <div className="flex h-screen">
+        <Drawer />
+        <div className="flex-1 flex flex-col h-screen overflow-hidden">
+          <div className="flex items-center bg-[#060606] z-10">
+            <Search />
+            <Navbar />
+            <Account />
+          </div>
+          <div className="flex-1 overflow-y-auto p-4">
+            <h1 className="text-xl text-white mb-4">Library</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {movies.map((movie) => (
+                <div key={movie.id} className="movie-card">
+                  <img 
+                    src={movie.posterUrl} 
+                    alt={`${movie.title} poster`} 
+                    className="w-full h-100 object-cover rounded"/>
+                  <h3 className="text-white mt-2 text-md font-[lato]">{movie.title}</h3>
+                  <p className="text-gray-400 text-xs">{movie.genre.join(', ')}</p>
+                  <p className="text-gray-400 text-xs">{movie.year}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- movie_dashboard/src/Components/account/account.tsx
+++ movie_dashboard/src/Components/account/account.tsx
@@ -4,7 +4,7 @@ const account = () => {
   return (
     <div>
         <div className='text-[#565656] flex items-center'>
-            <ul className='mt-10 pl-25 flex gap-6'>
+            <ul className='pl-25 flex gap-6'>
                 <div className='w-20 h-7 outline-red-500 outline-solid outline-1 rounded-lg focus:outline-none'>
                      <li><a href="" className='hover:text-white p-5'>Login</a></li>
                 </div>
@@ -13,7 +13,7 @@ const account = () => {
                </div>
             </ul>
             <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 items-center ml-16 mt-10 cursor-pointer hover:text-red-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 items-center ml-16 cursor-pointer hover:text-red-500">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </span>
