5041cbca85f42f4f3bc3c7ab22aa6112d2ba044f
Binada-SH <binadasasmika@gmail.com>
2025-05-20 19:24:49 +0530

    Hover color change effect adding

--- movie_dashboard/src/Components/drawer/drawer.tsx
+++ movie_dashboard/src/Components/drawer/drawer.tsx
@@ -8,19 +8,19 @@ const drawer = () => {
                 <h1 className="text-[18px] text-white text-left p-10 font-[jura] italic">Cine • <span className='text-red-500'>Streams</span></h1>
             </div>
             <ul className='list-none text-[#565656] leading-[2.8]'>
-                <div className="flex item-center pt-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10">
+                <div className="flex item-center pt-3 hover:text-red-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 ">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>
                     <span>
-                        <li><a href="#home" className='p-4 font-[lato]'>Home</a></li>
+                        <li><a href="#home" className='p-4 font-[lato] hover:text-red-500'>Home</a></li>
                     </span>
                 </div>
-                <div className='flex items-center'>
+                <div className='flex items-center hover:text-red-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2 ml-10 shrink-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
                     </svg>
-                     <span><li><a href="#new" className='p-4 font-[lato]'>Discover</a></li></span>
+                     <span><li><a href="#new" className='p-4 font-[lato] hover:text-red-500'>Discover</a></li></span>
                 </div>
             </ul>
             <hr className='w-33 h-1 ml-10 rounded-lg border-[#303030] my-6' />
@@ -28,36 +28,36 @@ const drawer = () => {
             <h4 className='text-white text-left  pt-4 p-10 font-[lato]'>Library
             </h4>
             <ul className='list-none text-[#565656] leading-[2.8]'>
-                <div className='flex'>
+                <div className='flex hover:text-red-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                     <span>
-                        <li><a href=""className='p-4 font-[lato]'>Recent</a></li>
+                        <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Recent</a></li>
                         </span>
                 </div>
-                <div className='flex '>
+                <div className='flex hover:text-red-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                     </svg>
                     <span>
-                        <li><a href=""className='p-4 font-[lato]'>Bookmarked</a></li>
+                        <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Bookmarked</a></li>
                     </span>
                 </div>
-                <div className='flex'>
+                <div className='flex hover:text-red-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
                     <span>
-                        <li><a href=""className='p-4 font-[lato]'>Liked</a></li>
+                        <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Liked</a></li>
                     </span>
                 </div>
-                <div className='flex'>
+                <div className='flex hover:text-red-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                     </svg>
                     <span>
-                        <li><a href=""className='p-4 font-[lato]'>Downloaded</a></li>
+                        <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Downloaded</a></li>
                     </span>
                 </div>  
             </ul>
@@ -68,29 +68,29 @@ const drawer = () => {
                 General
             </h4>
             <ul className='list-none text-[#565656] leading-[2.8]'>
-                <div className='flex'>
+                <div className='flex hover:text-red-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                     <span>
-                        <li><a href=""className='p-4 font-[lato]'>Settings</a></li>
+                        <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Settings</a></li>
                     </span>
                 </div>
-            <div className='flex'>
+            <div className='flex hover:text-red-500'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                 </svg>
                 <span>
-                    <li><a href=""className='p-4 font-[lato]'>Help</a></li>
+                    <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Help</a></li>
                 </span>
             </div>
-            <div className='flex'>
+            <div className='flex hover:text-red-500'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mt-2.5 ml-10 shrink-0">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
                 </svg>
                 <span>
-                    <li><a href=""className='p-4 font-[lato]'>Logout</a></li>
+                    <li><a href=""className='p-4 font-[lato] hover:text-red-500'>Logout</a></li>
                 </span>
             </div>
             </ul>
