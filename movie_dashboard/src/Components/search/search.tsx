f3b11d48ba793da05282ed64373acac5aa2a349e
Binada-SH <binadasasmika@gmail.com>
2025-05-20 20:47:49 +0530

    Searchbar placeholder character spacing update for UI/UX

--- movie_dashboard/src/Components/search/search.tsx
+++ movie_dashboard/src/Components/search/search.tsx
@@ -17,7 +17,7 @@ const search = () => {
                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                         <span>
-                            <input type="text" id="search" name="search" placeholder="Search Any Movie" className='pl-6 font-[lato] tracking-wider border-none outline-none'/>
+                            <input type="text" id="search" name="search" placeholder="Search Any Movie" className='pl-6 font-[lato] tracking-widest border-none outline-none'/>
                         </span>
                 
             </div>
